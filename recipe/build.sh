ff7eadffae8d3be568678a5087532c5c6f581655
Alberto Scolari <alberto.scolari88@gmail.com>
2025-11-26 00:10:27 +0100

    porting recipe to rattler-build and adding script-based launcher

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,8 @@
 # Script inspired by the contents of the sbt debian packages.
 # Debian packages: https://dl.bintray.com/sbt/debian/
 
-mkdir -p $PREFIX/share/sbt/bin
-mkdir -p $PREFIX/bin
+mkdir -p ${PREFIX}/share/sbt/bin
+mkdir -p ${PREFIX}/bin
 
-cp ./bin/sbt ./bin/sbt-launch.jar $PREFIX/share/sbt/bin/
-
-ln -rs $PREFIX/share/sbt/bin/sbt $PREFIX/bin/sbt
-ln -rs $PREFIX/share/sbt/bin/sbt-launch.jar $PREFIX/bin/sbt-launch.jar
-
+cp ./bin/sbt ./bin/sbt-launch.jar ${PREFIX}/share/sbt/bin/
+cp ${RECIPE_DIR}/sbt ${PREFIX}/bin/sbt
