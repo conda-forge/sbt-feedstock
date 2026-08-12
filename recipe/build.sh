da79295912c7e3958dcc2e3493b8a4cab2855244
​Andrzej Ressel <github@andrzejressel.pl>
2026-08-13 00:36:45 +0200

    Fix running sbt on Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,6 @@
 mkdir -p ${PREFIX}/share/sbt/bin
 mkdir -p ${PREFIX}/bin
 
-cp ./bin/sbt ./bin/sbt-launch.jar ${PREFIX}/share/sbt/bin/
+cp ./bin/sbt ./bin/sbt.bat ./bin/sbt-launch.jar ./bin/sbtn-x86_64-pc-win32.exe ${PREFIX}/share/sbt/bin/
 cp ${RECIPE_DIR}/sbt ${PREFIX}/bin/sbt
 cp ${RECIPE_DIR}/sbt.bat ${PREFIX}/bin/sbt.bat
