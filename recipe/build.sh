f24ed8687d896e0ec47d2470a89c76bf890c0a98
​Andrzej Ressel <github@andrzejressel.pl>
2026-08-13 12:20:37 +0200

    Fix running sbt on Windows (#63)
    * Fix running sbt on Windows
    * Increment build number from 0 to 1

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,15 @@
 mkdir -p ${PREFIX}/share/sbt/bin
 mkdir -p ${PREFIX}/bin
 
-cp ./bin/sbt ./bin/sbt-launch.jar ${PREFIX}/share/sbt/bin/
+cp ./bin/sbt ./bin/sbt.bat ./bin/sbt-launch.jar ${PREFIX}/share/sbt/bin/
+
+# copy all the GraalVM-generated, native launchers
+for file in ./bin/sbtn-*; do
+    # skip iteration if the file does not exist (in case the shell could not expand to an existing file name)
+    [ -e "${file}" ] || continue
+    
+    cp ${file} ${PREFIX}/share/sbt/bin/
+done
+
 cp ${RECIPE_DIR}/sbt ${PREFIX}/bin/sbt
 cp ${RECIPE_DIR}/sbt.bat ${PREFIX}/bin/sbt.bat
